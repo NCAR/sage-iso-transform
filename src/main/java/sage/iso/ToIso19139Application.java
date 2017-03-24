839f35932a2c57a8c5e687365a28f06cc4e1bdcc
nathan-hook <nhook@ucar.edu>
2017-03-24 16:49:49 -0600

    Fixing issue where the output file was created before the transform actually occurred and if the transform failed for some reason, the output file wasn't deleted.
    Changed behavior now so that the File is only created/written to if the xml transform is successful.

--- src/main/java/sage/iso/ToIso19139Application.java
+++ src/main/java/sage/iso/ToIso19139Application.java
@@ -7,14 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import sage.iso.transformer.ToIso19139Transformer;
 
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.util.UUID;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class ToIso19139Application implements CommandLineRunner {
 
@@ -25,8 +24,6 @@ public class ToIso19139Application implements CommandLineRunner {
 
     @Value("${output}")
     private String outputFileLocation;
-
-    private UUID uuid = UUID.randomUUID();
 
     private static String TO_ISO_19139_OLD_ISO_FROM_NEW_ISO = "toISO19139.xsl";
 
@@ -45,9 +42,16 @@ public class ToIso19139Application implements CommandLineRunner {
             ToIso19139Transformer transformer = new ToIso19139Transformer(this.createSaxonTransformer());
 
             StreamSource sourceXml = this.createStreamSource(this.inputFileLocation);
-            StreamResult resultXml = this.createStreamResult(this.outputFileLocation);
+
+            ByteArrayOutputStream resultByteArrayOutputStream = new ByteArrayOutputStream();
+            StreamResult resultXml = new StreamResult(resultByteArrayOutputStream);
 
             transformer.transform(sourceXml, resultXml);
+
+            File resultFile = this.createOutputFileWithDirectories(this.outputFileLocation);
+            try(OutputStream outputStream = new FileOutputStream(resultFile)) {
+                resultByteArrayOutputStream.writeTo(outputStream);
+            }
 
             this.logStatus("COMPLETED");
 
@@ -104,6 +108,6 @@ public class ToIso19139Application implements CommandLineRunner {
 
     public void logStatus(String status, Throwable throwable) {
 
-        log.info("{} {} {} {}", uuid.toString(), this.inputFileLocation, this.outputFileLocation, status, throwable);
+        log.info("{} {} {}", this.inputFileLocation, this.outputFileLocation, status, throwable);
     }
 }
